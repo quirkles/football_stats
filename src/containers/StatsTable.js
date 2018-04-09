6a63f5a25d88453d11bc7ef21f69bf8a8d4e78e3
njwdev <nwwebd@gmail.com>
2018-04-10 01:23:52 +0200

    add table

--- src/containers/StatsTable.js
+++ src/containers/StatsTable.js
@@ -18,7 +18,26 @@ class StatsTable extends Component {
       return (
           <div className='statsTable'>
             <p>Stats table here</p>
-            {/* Stats table goes here. It should render automatically. */}
+            <table border= "1px solid black">
+		<thead>
+			<th> Player Number </th>
+            <th> Player Name </th>
+			<th> Appearances</th>
+			<th> Goals </th>
+			<th> Assists </th>
+			<th> Man of the Match </th>
+{/* <!-- More categories possible - e.g Goal contributions, goals per game etc - perhaps under an advanced tab? --> */}
+	</thead>
+	<tr> 
+		<td> {this.state.number} </td>
+        <td> {this.state.name} </td>
+		<td> {this.state.stats[0].apps} </td>
+		<td> {this.state.stats[0].goals} </td>
+		<td> {this.state.stats[0].assists} </td>
+		<td> {this.state.stats[0].motm} </td>
+
+	</tr>
+	</table>
           </div>
       )
   }
