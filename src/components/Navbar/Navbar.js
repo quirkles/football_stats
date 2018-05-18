a8e01d815b8641f803e1fb0dbaf1b6a4b5276290
jwellwood <joemadriddev@gmail.com>
2018-05-18 21:27:46 +0200

    nav adjustment

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -22,18 +22,33 @@ class NavBar extends Component {
   render() {
     return (
       <div>
-        <Navbar light className="navbarMain navbar-brand">
-          <NavLink to="/" exact className="navbar-brand">
-            <i className="far fa-futbol"></i>
-          </NavLink>
-          <NavLink to="/home/" exact className="nav-link">
+        <Navbar light 
+          className={classes.navbarBrand} 
+          className={classes.navbarMain}>
+            <NavLink 
+              to="/" 
+              exact 
+              className="navbar-brand">
+              <i style={{color:'white'}}className="far fa-futbol"></i>
+            </NavLink>
+          <NavLink 
+            to="/home/" 
+            exact 
+            className={classes.navLink}>
             <span className='navel'> Home </span> <i className="fas fa-home"></i>
           </NavLink>
-          <NavLink to="/squad/" exact className="nav-link">
-          <span className='navel'> Squad </span> <i className="fas fa-users"></i>
+          <NavLink 
+            to="/squad/" 
+            exact 
+            className={classes.navLink}>
+            <span 
+              className='navel'> Squad </span> 
+            <i className="fas fa-users"></i>
           </NavLink>
-          <NavLink to="/stats/" exact className="nav-link">
-          <span className='navel'> Stats </span> <i className="fas fa-chart-line"></i>
+          <NavLink 
+            to="/stats/" 
+            exact className={classes.navLink}>
+            <span className='navel'> Stats </span> <i className="fas fa-chart-line"></i>
           </NavLink>
           <NavbarToggler onClick={this.toggleNavbar} className="toggler" />
           <Collapse isOpen={!this.state.collapsed} navbar>
