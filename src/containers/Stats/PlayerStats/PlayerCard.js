fad994f56e1255e190b1118f2e62371c0ea97ccd
jwellwood <joemadriddev@gmail.com>
2018-05-20 00:39:25 +0200

    carousel fix

--- src/containers/Stats/PlayerStats/PlayerCard.js
+++ src/containers/Stats/PlayerStats/PlayerCard.js
@@ -23,7 +23,7 @@ class PlayerCard extends Component {
         </div>
         <hr />
         <div className={classes.cardImage}>
-          <p id="squadNumber">#{this.state.number}</p>
+          <p className={classes.squadNumber}>#{this.state.number}</p>
           <img
             className={classes.image}
             src="https://images.vexels.com/media/users/3/129733/isolated/preview/a558682b158debb6d6f49d07d854f99f-casual-male-avatar-silhouette-by-vexels.png"
