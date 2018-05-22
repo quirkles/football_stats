f4b4763f305bd02b75f4f1e856e5945d13d55fdb
jwellwood <joemadriddev@gmail.com>
2018-05-23 01:32:41 +0200

    small table change

--- src/containers/Stats/StatsTable/StatsTable.js
+++ src/containers/Stats/StatsTable/StatsTable.js
@@ -75,7 +75,7 @@ render () {
       resizable: false
     },
     {
-      Header: <i className="fas fa-user-friends"></i>,
+      Header: <i class="far fa-handshake"></i>,
       accessor: 'assists',
       width: 40,
       resizable: false
